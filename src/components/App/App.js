b260fefe720fb8004515829564f8d1f7f6292490
oksana <opashkovska85@gmail.com>
2021-08-09 11:51:31 +0200

    add app structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+
+const App = () => (
+    <div>
+        <h1>Important Tasks</h1>
+        <InputItem />
+        <ItemList />
+        <Footer />
+    </div>);
+
+    export default App;
