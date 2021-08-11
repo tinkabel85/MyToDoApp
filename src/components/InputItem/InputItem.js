5a0aa8e8f6185197ca2fec67728268f3a15c789b
oksana <opashkovska85@gmail.com>
2021-08-11 11:17:19 +0200

    add component material ui

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>InputItem</span>);
-
+const InputItem = () => (<div>
+    <TextField
+      label="Add a new task"
+      id="margin-dense"
+      defaultValue="Default Value"
+      helperText="Please try to keep it precise and short"
+      margin="dense"
+    />
+  </div>);
 export default InputItem;
