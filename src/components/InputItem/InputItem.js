a8146b07cbc1004afe8f37632745dc77f525196f
oksana <opashkovska85@gmail.com>
2021-08-12 20:42:48 +0200

    add all styles

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import styles from './InputItem.module.css';
 import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
 
-const InputItem = () => (<div>
-    <TextField
-      label="Add a new task"
-      id="margin-dense"
-      defaultValue="Default Value"
-      helperText="Please try to keep it precise and short"
-      margin="dense"
+const useStyles = makeStyles({
+  root: {
+    '& label': {
+      color: '#e3d8d8',
+      letterSpacing: '5px',
+      fontSize: '20px',
+    },
+    '& .MuiInput-underline:after': {
+        borderColor: '#c4c4c4',
+      },
+    '& label.Mui-focused': {
+      color: '#c4c4c4',
+    },
+    '&:hover' : {
+      backgroundColor: '#fcfcfb',
+    },
+  },
+});
+
+export default function CustomizedInput() {
+  const classes = useStyles();
+  return ( <div className = {classes.root}>
+    <TextField id = "standard-full-width"
+    style = {{margin: 8}}
+    label = "What needs to be done?"
+    fullWidth
+    margin = "normal"
     />
-  </div>);
-export default InputItem;
+    </div>
+  );
+}
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
 
 
-const Itemlist = ({items}) => (<ul>
-      {items.map(item => <li key={item.value}>
-          <Item value={item.value}isDone={item.isDone} /></li>)}
-  </ul>);
+const Itemlist = ({items}) =>
+(<div className={styles.wrap}>
+      {items.map(item =>
+        <Item key={item.value}
+          Item value={item.value}
+          isDone={item.isDone} />
+        )}
+  </div>);
 
   export default Itemlist;
