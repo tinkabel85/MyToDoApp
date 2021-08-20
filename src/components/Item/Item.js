c7ab157bc4a2729d5b879d869a073781f272e1ec
oksana <opashkovska85@gmail.com>
2021-08-20 13:51:29 +0200

    demo lifecycle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,26 +5,61 @@ import PropTypes from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
-const Item = ({value, isDone, id, onClickDone, onClickDelete}) => (
-  <div className={styles.wrap}>
-      <Checkbox
-          color="default"
-          inputProps={{ 'aria-label': 'checkbox with default color' }}
-          onClick={()=>onClickDone(id)}
-        />
-  <span className={
-    classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-      })
-    }>
-      {value}
-  </span>
-         <DeleteOutlinedIcon className={styles.btn} color="disabled"
-         onClick={()=> onClickDelete(id)} />
+class Item extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
 
- </div>
-);
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    const {value, isDone, id, onClickDone, onClickDelete} = this.props;
+
+    return (<div className={styles.wrap}>
+          <Checkbox
+              color="default"
+              inputProps={{ 'aria-label': 'checkbox with default color' }}
+              onClick={()=>onClickDone(id)}
+            />
+          <span className={
+            classnames({
+                [styles.item]: true,
+                [styles.done]: isDone
+              })
+            }>
+              {value}
+          </span>
+            <DeleteOutlinedIcon className={styles.btn} color="disabled"
+              onClick={()=> onClickDelete(id)} />
+        </div>);
+  }
+}
+// const Item = ({value, isDone, id, onClickDone, onClickDelete}) => (
+//   <div className={styles.wrap}>
+//       <Checkbox
+//           color="default"
+//           inputProps={{ 'aria-label': 'checkbox with default color' }}
+//           onClick={()=>onClickDone(id)}
+//         />
+//   <span className={
+//     classnames({
+//         [styles.item]: true,
+//         [styles.done]: isDone
+//       })
+//     }>
+//       {value}
+//   </span>
+//          <DeleteOutlinedIcon className={styles.btn} color="disabled"
+//          onClick={()=> onClickDelete(id)} />
+//
+//  </div>
+// );
 
 Item.propTypes = {
   value: PropTypes.string.isRequired,
