fd61ba9dcb15a832d1aa1f550653c314d0ac3362
oksana <opashkovska85@gmail.com>
2022-07-01 11:57:27 +0200

    fix demo link and add editing of tasks

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,114 +8,114 @@ import ReactPaginate from 'react-paginate';
 const octokit = new Octokit();
 
 const About = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [repoList, setRepoList] = useState([]);
-    const [requestFailed, setRequestFailed] = useState(false);
-    const [error, setError] = useState('');
-    const [userInfo, setUserInfo] = useState({});
-    const [pageNumber, setPageNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [repoList, setRepoList] = useState([]);
+  const [requestFailed, setRequestFailed] = useState(false);
+  const [error, setError] = useState('');
+  const [userInfo, setUserInfo] = useState({});
+  const [pageNumber, setPageNumber] = useState(0);
 
-    useEffect(() => {
-      octokit.repos.listForUser({
-          username: 'tinkabel85'
-        })
-        .then(response => {
-          setRepoList(response.data);
-          setIsLoading(false);
-        })
-        .catch(err => {
-          setRequestFailed(true);
-          setIsLoading(false);
-          setError('Something went wrong...');
-        });
+  useEffect(() => {
+    octokit.repos.listForUser({
+      username: 'tinkabel85'
+    })
+      .then(response => {
+        setRepoList(response.data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setRequestFailed(true);
+        setIsLoading(false);
+        setError('Something went wrong...');
+      });
 
-      octokit.users.getByUsername({
-          username: 'tinkabel85'
-        })
-        .then(response => {
-          setUserInfo(response.data);
-          setIsLoading(false);
-        })
-        .catch(err => {
-          setRequestFailed(true);
-          setIsLoading(false);
-          setError('Something went wrong...');
-        })
+    octokit.users.getByUsername({
+      username: 'tinkabel85'
+    })
+      .then(response => {
+        setUserInfo(response.data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setRequestFailed(true);
+        setIsLoading(false);
+        setError('Something went wrong...');
+      })
 
-    }, []);
+  }, []);
 
 
 
-const reposPerPage = 4;
-const reposSeen = pageNumber * reposPerPage;
-const displayRepos = repoList.slice(reposSeen, reposSeen + reposPerPage);
-const pageCount = Math.ceil(repoList.length / reposPerPage);
-const changePage = ({selected}) => {
+  const reposPerPage = 4;
+  const reposSeen = pageNumber * reposPerPage;
+  const displayRepos = repoList.slice(reposSeen, reposSeen + reposPerPage);
+  const pageCount = Math.ceil(repoList.length / reposPerPage);
+  const changePage = ({ selected }) => {
     setPageNumber(selected)
-}
+  }
 
 
-    return (
-        <div className={styles.wrap}>
-          { isLoading ?  <CircularProgress /> :
-            <div>
+  return (
+    <div className={styles.wrap}>
+      {isLoading ? <CircularProgress /> :
+        <div>
           {!isLoading &&
             <div>
-              {!requestFailed  &&
+              {!requestFailed &&
                 <div className={styles.header}>
-                    <div>
-                        <img className={styles.avatar} src={userInfo.avatar_url} alt={userInfo.name} />
-                      </div>
-                      <div className={styles.info}>
-                          <h1 className={styles.info_name}>{userInfo.name}</h1>
-                          <img className={styles.location} src={ location } alt='location'></img>
-                          <div className={styles.info_city}> {userInfo.location}</div>
-                          <div className={styles.info_bio}> {userInfo.bio}</div>
-                          <a className={styles.link} href={userInfo.html_url} target='blank' >Here is my GitHub account.</a>
-                      </div>
-                </div> }
-            <ul className={styles.repos_all}>
-                  {displayRepos.map(repo => (<li key={repo.id} className={styles.repos}>
-                    <div className={styles.repos_name}>
-                      <a href={repo.html_url} target='blank' className = {styles.repos_link}>
-                        {repo.name}
-                        </a>
-                      <a target='_blank' rel = 'noreferrer' className = {styles.repos_host}
-                      href={repo.name === 'todo_app' ? 'https://about-tinkabel.netlify.app/' : `https://tinkabel85.github.io/${repo.name}`}>
+                  <div>
+                    <img className={styles.avatar} src={userInfo.avatar_url} alt={userInfo.name} />
+                  </div>
+                  <div className={styles.info}>
+                    <h1 className={styles.info_name}>{userInfo.name}</h1>
+                    <img className={styles.location} src={location} alt='location'></img>
+                    <div className={styles.info_city}> {userInfo.location}</div>
+                    <div className={styles.info_bio}> {userInfo.bio}</div>
+                    <a className={styles.link} href={userInfo.html_url} target='blank' >Here is my GitHub account.</a>
+                  </div>
+                </div>}
+              <ul className={styles.repos_all}>
+                {displayRepos.map(repo => (<li key={repo.id} className={styles.repos}>
+                  <div className={styles.repos_name}>
+                    <a href={repo.html_url} target='blank' className={styles.repos_link}>
+                      {repo.name}
+                    </a>
+                    <a target='_blank' rel='noreferrer' className={styles.repos_host}
+                      href={repo.name === 'todo_app' ? 'https://about-tinkabel.netlify.app/' : repo.name === 'City4mini' ? 'https://city4mini.herokuapp.com/' : `https://tinkabel85.github.io/${repo.name}`}>
                       Demo</a>
+                  </div>
+                  <div className={styles.repos__info}>
+                    <div className={styles.lang}>
+                      <span className={[styles.circle, styles[repo.language]].join(' ')}></span>
+                      <span className={styles.repos_lang}> {repo.language}</span>
                     </div>
-                    <div className={styles.repos__info}>
-                        <div className= {styles.lang}>
-                          <span className={[styles.circle, styles[repo.language]].join(' ')}></span>
-                          <span className = {styles.repos_lang}> {repo.language}</span>
-                      </div>
-                          <span className={styles.star}>{repo.stargazers_count}</span>
-                          <span className={styles.fork}>{repo.forks_count}</span>
-                          <span className = {styles.repos_date}> Updated on { new Date( repo.updated_at).toLocaleDateString('en-US',
-                            {year: 'numeric', month: 'short', day: 'numeric'})}
-                            </span>
-                    </div>
-                  </li>))}
-            </ul>
-            <ReactPaginate
-                previousLabel = {'Previous'}
-                nextLabel = {'Next'}
-                pageCount = {pageCount}
-                onPageChange = {changePage}
-                containerClassName = {styles.paginationBttns}
-                previousLinkClassName = {styles.previousBttn}
-                nextLinkClassName = {styles.nextBttn}
-                disabledClassName = {styles.paginationDisabled}
-                activeClassName = {styles.paginationActive}
-            />
-          </div>
-        }
+                    <span className={styles.star}>{repo.stargazers_count}</span>
+                    <span className={styles.fork}>{repo.forks_count}</span>
+                    <span className={styles.repos_date}> Updated on {new Date(repo.updated_at).toLocaleDateString('en-US',
+                      { year: 'numeric', month: 'short', day: 'numeric' })}
+                    </span>
+                  </div>
+                </li>))}
+              </ul>
+              <ReactPaginate
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={styles.paginationBttns}
+                previousLinkClassName={styles.previousBttn}
+                nextLinkClassName={styles.nextBttn}
+                disabledClassName={styles.paginationDisabled}
+                activeClassName={styles.paginationActive}
+              />
+            </div>
+          }
         </div>
       }
-            {requestFailed &&  <h2 className={styles.error}> {error} </h2>}
+      {requestFailed && <h2 className={styles.error}> {error} </h2>}
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default About;
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css';
 import PropTypes from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
-const Item = ({value, isDone, id, visible, onClickDone, onClickDelete}) => (
-  <div className={styles.wrap}>
-      <Checkbox
+const Item = ({ value, isDone, id, visible, onClickDone, onClickDelete }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(value);
+
+  const onClickEdit = value => {
+    setEditing(true);
+    console.log('edits');
+  }
+
+  const onClickEditDone = event => {
+    console.log('editing is done');
+    if (event.keyCode === 13) { //submit
+      setEditing(false);
+    }
+  }
+  const onClickEditChange = event => {
+    const changedInput = event.target.value;
+    console.log(event.target.value);
+    console.log('edit saved');
+    setInputValue(changedInput);
+  }
+
+  let viewMode = {}
+  let editMode = {}
+
+  if (isEditing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none'
+  }
+
+  return (
+    <div className={styles.wrap}>
+      <div style={viewMode} >
+        <Checkbox
           color="default"
           checked={isDone}
           inputProps={{ 'aria-label': 'checkbox with default color' }}
-          onClick={()=>onClickDone(id)}
+          onClick={() => onClickDone(id)}
         />
-  <span className={
-    classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-      })
-    }>
-      {value}
-  </span>
-         <DeleteOutlinedIcon className={styles.btn} color="disabled"
-         onClick={()=> onClickDelete(id)} />
+        <span className={
+          classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+          })
+        }
+          onDoubleClick={() => onClickEdit(value)}>
+          {inputValue}
+        </span>
+        <DeleteOutlinedIcon className={styles.btn} color="disabled"
+          onClick={() => onClickDelete(id)} />
 
- </div>
-);
+      </div>
 
+      <input type='text'
+        onKeyDown={(event) => onClickEditDone(event)}
+        onChange={(event) => onClickEditChange(event)}
+        style={editMode}
+        value={inputValue} />
+    </div>
+
+  );
+}
 Item.propTypes = {
   value: PropTypes.string.isRequired,
   isDone: PropTypes.bool.isRequired,
